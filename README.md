c01afef1030c2609b8af6b8c92c83f211346b5f5
Robert <robert.balulis@hotmail.com>
2023-04-05 03:54:08 +0200

    made the code prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# A program to visualize csv data from HMI/PLC (has only been tested on beijer ix HMI)
+# A program to visualize csv data from HMI/PLC 
+It has only been tested on beijer ix HMI
  
--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-from tkinter import *
-from tkinter import filedialog
-import plotly.express as px
-import pandas as pd
-import tkinter as tk
-import dash
-from dash import dcc
-from dash import html
-from dash.dependencies import Input, Output
+from tkinter import IntVar, filedialog, messagebox
 import threading
-import webbrowser
-import time
-import customtkinter
-import os
-from PIL import Image
-import moviepy.editor as mp
-import subprocess
 from os import startfile
+import webbrowser
 
-customtkinter.set_appearance_mode("dark") 
+import customtkinter
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+from PIL import Image
+from waitress import serve
+
+
+customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 
-EXTENSION = "csv"
+HELP_TEXT_FILE = "help_text.txt"
+HELP_VIDEO_FILE = "Help.mp4"
+BACKGROUND_IMAGE = "bg.jpg"
 
-checked = []
+components_checked = []
 
 filenames = []
 
-
-def browseFiles():
+def browseFiles(app_instance):
     global filename
     filename = filedialog.askopenfilename(initialdir = "/",
                                           title = "Select a File",
@@ -35,58 +32,57 @@ def browseFiles():
                                                         "*.csv*"),
                                                        ("all files",
                                                         "*.*")))
-    
 
-    
 
-    filenames.append(filename)
-    after_file_selected()
-    
+    if filename.endswith("csv"):
+        filenames.append(filename)
+        after_file_selected(app_instance)
+
+    else:
+        messagebox.showinfo("Invalid file type", "Please select a CSV file.")
+
     return filenames
 
 
-def after_file_selected():
-    global frame
-    global header
+def after_file_selected(app_instance):
+    global data
+    global components_name
     dfs = []
 
     def custom_date_parser(date_str):
         return pd.to_datetime(date_str, format='%m/%d/%Y %I:%M:%S %p')
-    
+
     for filename in filenames:
-         df = pd.read_csv(filename, parse_dates=['Time'], date_parser=custom_date_parser)
-         dfs.append(df)
-    frame = pd.concat(dfs, axis=0, ignore_index=True)
-    
+        dataframe = pd.read_csv(filename, parse_dates=['Time'], date_parser=custom_date_parser)
+        dfs.append(dataframe)
+    data = pd.concat(dfs, axis=0, ignore_index=True)
 
-    frame = frame.dropna()
-    print(frame)
-    frame['Time'] = pd.to_datetime(frame['Time'])
+    data = data.dropna()
 
+    data['Time'] = pd.to_datetime(data['Time'])
 
-    header = pd.read_csv(filename, index_col=0, nrows=0).columns.tolist()
-    header = header[1:]
-    
-    app.create_dynamic_checkboxes(header)
-    return frame, header
+    components_name = pd.read_csv(filename, index_col=0, nrows=0).columns.tolist()
+    components_name = components_name[1:]
+
+    app_instance.create_dynamic_checkboxes(components_name)
+    return data, components_name
 
 
-def create_dash_app(header, frame, checked):
-    fig = px.line(frame, x="Time", y=header)
+def create_dash_app(components_name, data, checked):
+    fig = px.line(data, x="Time", y=components_name)
 
     app = dash.Dash(__name__)
-    time.sleep(1)
     webbrowser.open_new('http://127.0.0.1:8050/')
 
 
     app.layout = html.Div([
         dcc.DatePickerRange(
             id='date-range-picker',
-            min_date_allowed=frame['Time'].min(),
-            max_date_allowed=frame['Time'].max(),
-            initial_visible_month=frame['Time'].min(),
-            start_date=frame['Time'].min().date(),
-            end_date=frame['Time'].max().date()
+            min_date_allowed=data['Time'].min(),
+            max_date_allowed=data['Time'].max(),
+            initial_visible_month=data['Time'].min(),
+            start_date=data['Time'].min().date(),
+            end_date=data['Time'].max().date()
         ),
         html.Div(id='hour-range-slider-output'),
         dcc.RangeSlider(
@@ -114,9 +110,9 @@ def create_dash_app(header, frame, checked):
     [Input('date-range-picker', 'start_date'),
      Input('date-range-picker', 'end_date'),
      Input('hour-range-slider', 'value')]
-    )   
+    )
 
-    
+
     def update_graph(start_date, end_date, hour_range):
         start_date = pd.to_datetime(start_date)
         end_date = pd.to_datetime(end_date)
@@ -124,22 +120,23 @@ def create_dash_app(header, frame, checked):
         start_time = start_date.replace(hour=hour_range[0])
         end_time = end_date.replace(hour=hour_range[1])
 
-        filtered_data = frame[(frame['Time'] >= start_time) & (frame['Time'] <= end_time)]
-        fig = px.line(filtered_data, x='Time', y=header)
+        filtered_data = data[(data['Time'] >= start_time) & (data['Time'] <= end_time)]
+        fig = px.line(filtered_data, x='Time', y=components_name)
 
-        selected_columns = [header[i] for i in checked]
+        selected_columns = [components_name[i] for i in checked]
         fig = px.line(filtered_data, x='Time', y=selected_columns)
 
         return fig
 
-    
-    app.run_server(debug=False)
+    serve(app.server, host='127.0.0.1', port=8050)
 
 
 def show_graph():
-    dash_thread = threading.Thread(target=create_dash_app, args=(header, frame, checked))
-    print(checked)
-    dash_thread.start()
+    if components_checked:
+        dash_thread = threading.Thread(target=create_dash_app, args=(components_name, data, components_checked))
+        dash_thread.start()
+    else:
+        messagebox.showinfo("No component selected", "Please select atleast one component to plot.")
 
 
 class ToplevelWindow(customtkinter.CTkToplevel):
@@ -148,8 +145,9 @@ class ToplevelWindow(customtkinter.CTkToplevel):
         self.geometry("700x300")
         self.resizable(False, False)
         self.title("LMT csv visualizer")
-        with open("help_text.txt", "r") as f:
-            help_text = f.read()
+
+        with open(HELP_TEXT_FILE, "r",encoding="utf-8") as text_file:
+            help_text = text_file.read()
 
         self.label = customtkinter.CTkLabel(self, text=help_text,justify="left")
         self.label.pack(padx=20, pady=20)
@@ -157,10 +155,9 @@ class ToplevelWindow(customtkinter.CTkToplevel):
         self.button_show_graph = customtkinter.CTkButton(master=self, command=self.play_video, text="See video tutorial")
         self.button_show_graph.pack(pady=10, padx=10)
 
-    def play_video(self):
-        # Load the video file using moviepy
-        startfile("Help.mp4")
 
+    def play_video(self):
+        startfile(HELP_VIDEO_FILE)
 
 
 class App(customtkinter.CTk):
@@ -170,18 +167,15 @@ class App(customtkinter.CTk):
         self.checkbox_vars = []
         self.toplevel_window = None
 
-        self.geometry("400x780")
+        self.geometry("400x500")
         self.title("LMT csv visualizer")
 
         self.resizable(False, False)
 
-
-        current_path = os.path.dirname(os.path.realpath(__file__))
-        self.bg_image = customtkinter.CTkImage(Image.open(current_path + "\\bg.jpg"),
+        self.bg_image = customtkinter.CTkImage(Image.open(BACKGROUND_IMAGE),
                                                size=(400, 780))
         self.bg_image_label = customtkinter.CTkLabel(self, image=self.bg_image)
         self.bg_image_label.place(x=0, y=0, relwidth=1, relheight=1)
-
 
         self.frame_1 = customtkinter.CTkFrame(master=self, bg_color="transparent")
         self.frame_1.place(relx=0.5, rely=0.5, anchor="center")
@@ -189,11 +183,10 @@ class App(customtkinter.CTk):
         self.frame_1 = customtkinter.CTkFrame(master=self)
         self.frame_1.pack(pady=20, padx=40, fill="both", expand=True)
 
+        main_label = customtkinter.CTkLabel(master=self.frame_1, justify=customtkinter.LEFT, text="Visualize csv data",font=customtkinter.CTkFont(size=20, weight="bold"),bg_color="transparent")
+        main_label.pack(pady=10, padx=10)
 
-        Lable1 = customtkinter.CTkLabel(master=self.frame_1, justify=customtkinter.LEFT, text="Visualize csv data",font=customtkinter.CTkFont(size=20, weight="bold"),bg_color="transparent")
-        Lable1.pack(pady=10, padx=10)
-
-        button_explore = customtkinter.CTkButton(master=self.frame_1, command=browseFiles, text="Browse files")
+        button_explore = customtkinter.CTkButton(master=self.frame_1, command=lambda: browseFiles(self), text="Browse files")
         button_explore.pack(pady=10, padx=10)
 
         button_show_graph = customtkinter.CTkButton(master=self.frame_1, command=show_graph, text="Show graph")
@@ -205,48 +198,43 @@ class App(customtkinter.CTk):
         button_exit = customtkinter.CTkButton(master=self.frame_1, command=exit, text="Exit")
         button_exit.pack(pady=10, padx=10)
 
-        
 
-
-    def create_dynamic_checkboxes(self, header):
+    def create_dynamic_checkboxes(self, components_name):
         self.scrollable_frame = customtkinter.CTkScrollableFrame(master=self.frame_1, height=200)
         self.scrollable_frame.pack(fill="both", expand=True)
- 
-        Lable2 = customtkinter.CTkLabel(master=self.scrollable_frame, justify=customtkinter.LEFT, text="Select what component(s) to visualize",font=customtkinter.CTkFont(size=14, weight="bold"))
-        Lable2.pack(pady=10, padx=10)
 
-        for idx, name in enumerate(header):
+        scroll_window_label = customtkinter.CTkLabel(master=self.scrollable_frame, justify=customtkinter.LEFT, text="Select what component(s) to visualize",font=customtkinter.CTkFont(size=14, weight="bold"))
+        scroll_window_label.pack(pady=10, padx=10)
+
+        for idx, name in enumerate(components_name):
             checkbox_var = IntVar()
             checkbox_1 = customtkinter.CTkCheckBox(master=self.scrollable_frame, text=name, variable=checkbox_var, command=lambda index=idx: self.on_checkbox_change(index))
 
             checkbox_1.pack(pady=10, padx=10)
             self.checkbox_vars.append(checkbox_var)
 
-        scrollbar = customtkinter.CTkScrollbar(master=self.scrollable_frame, orient=VERTICAL, command=self.scrollable_frame.yview)
-        self.scrollable_frame.configure(yscrollcommand=scrollbar.set)
-        scrollbar.pack(side=RIGHT, fill=Y, master=self.scrollable_frame)
 
     def on_checkbox_change(self,index):
-    
+
         if self.checkbox_vars[index].get() == 1:
-            checked.append(index)
+            components_checked.append(index)
         else:
-            checked.remove(index)
+            components_checked.remove(index)
 
 
     def open_toplevel(self):
         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-            self.toplevel_window = ToplevelWindow(self)  
+            self.toplevel_window = ToplevelWindow(self)
         else:
-            self.toplevel_window.focus() 
+            self.toplevel_window.focus()
 
         self.toplevel_window.lift()
 
 
-
-if __name__ == "__main__":
+def main():
     app = App()
     app.mainloop()
 
 
-
+if __name__ == "__main__":
+    main()
