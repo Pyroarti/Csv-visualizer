e208cc00f5be832af7cf2c0c0591a2ecbf8414e2
Robert <robert.balulis@hotmail.com>
2023-04-10 17:06:53 +0200

    made the code prettier

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+"""Main program to visualize csv data, can be tested on the example data in the folder "data" """
+
+import time
 from tkinter import IntVar, filedialog, messagebox
 import threading
 import os
 from os import startfile
 import signal
 import webbrowser
-import time
 
 import customtkinter
 import dash
@@ -19,19 +21,20 @@ from waitress import serve
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 
-HELP_TEXT_FILE = "help_text.txt"
-HELP_VIDEO_FILE = "Help.mp4"
-BACKGROUND_IMAGE = "bg.jpg"
+HELP_TEXT_FILE = r"UI_componentes\help_text.txt"
+HELP_VIDEO_FILE = r"UI_componentes\Tutorial.mp4"
+BACKGROUND_IMAGE = r"UI_componentes\backround.jpg"
 
 components_checked = []
-
 filenames = []
 
-# It promts the user to select a csv file and adds the csv files to a list for handling and plotting multiple csv data.
+
 def browse_files(app_instance):
-    
+    """It promts the user to select a csv file 
+    and adds the csv files to a list for handling and plotting multiple csv data."""
+
     initial_dir = os.getcwd()
-    
+
     filename = filedialog.askopenfilename(initialdir = initial_dir,
                                           title = "Select a File",
                                           filetypes = (("csv files",
@@ -50,17 +53,15 @@ def browse_files(app_instance):
     return filenames
 
 
-# Takes the csv data and makes them into dataframes and adds the componentes to a list for the user to select what to plot.
+
 def after_file_selected(app_instance):
+    """Takes the csv data and makes them into dataframes 
+    and adds the componentes to a list for the user to select what to plot."""
     global data
     global components_name
     dfs = []
 
-    def custom_date_parser(date_str):
-        return pd.to_datetime(date_str, format='%m/%d/%Y %I:%M:%S %p')
-     
-    for filename in filenames:
-        #dataframe = pd.read_csv(filename, parse_dates=['Time'], date_parser=custom_date_parser)
+    for filename in filenames: # Makes possible to select multiple csv files
         dataframe = pd.read_csv(filename)
         dfs.append(dataframe)
     data = pd.concat(dfs, axis=0, ignore_index=True)
@@ -71,29 +72,31 @@ def after_file_selected(app_instance):
 
     components_name = pd.read_csv(filename, index_col=0, nrows=0).columns.tolist()
     components_name = components_name[1:]
+
     # Makes checkboxes in the UI for each component in the csv file.
     app_instance.create_dynamic_checkboxes(components_name)
+
     return data, components_name
 
-# Makes the dash webserver and adds a calaneder and hour slider.
-def create_dash_app(components_name, data, checked):
 
-    fig = px.line(data, x="Time", y=components_name)
+def create_dash_app(components_name, data, checked):
+    """Makes the dash webserver and adds a calaneder and hour slider."""
 
     app = dash.Dash(__name__)
-
+    #Calender so the user can select what date to see the plottet data
     app.layout = html.Div([
-        dcc.DatePickerRange( # HTML for the calender so the user can select what date to see the plottet data
+        dcc.DatePickerRange(
             id='date-range-picker',
             min_date_allowed=data['Time'].min().date(),
             max_date_allowed=data['Time'].max().date(),
             initial_visible_month=data['Time'].min().date(),
             start_date=data['Time'].min().date(),
             end_date=data['Time'].max().date(),
-                    
+
         ),
+        #Hour slider so the user can select the hour of the day to see the plottet data.
         html.Div(id='hour-range-slider-output'),
-        dcc.RangeSlider( # HTML for the hour slider so the user can select the hour of the day to see the plottet data.
+        dcc.RangeSlider(
             id='hour-range-slider',
             min=0,
             max=23,
@@ -106,7 +109,7 @@ def create_dash_app(components_name, data, checked):
         )
     ])
 
-    
+
     @app.callback(
     Output('hour-range-slider-output', 'children'),
     [Input('hour-range-slider', 'value')]
@@ -122,13 +125,10 @@ def create_dash_app(components_name, data, checked):
      Input('hour-range-slider', 'value')]
     )
 
-    # Updates graph when the user changes the date or time.
+
     def update_graph(start_date, end_date, hour_range):
+        """Updates graph when the user changes the date or time."""
         start_date = pd.to_datetime(start_date)
-        print(start_date)
-        print(data)
-        print(data['Time'].min())
-        print(data['Time'].min().date())
         end_date = pd.to_datetime(end_date)
 
         start_time = start_date.replace(hour=hour_range[0])
@@ -141,23 +141,26 @@ def create_dash_app(components_name, data, checked):
         fig = px.line(filtered_data, x='Time', y=selected_columns)
 
         return fig
-    
 
-    # Starts the webserver with waitress
-    dash_thread = threading.Thread(target=serve, args=(app.server,), kwargs={'host': '127.0.0.1', 'port': 8050, '_quiet': True})
+
+    #Starts the webserver with waitress.
+    dash_thread = threading.Thread(target=serve, args=(app.server,),
+                                   kwargs={'host': '127.0.0.1', 'port': 8050, '_quiet': True})
     dash_thread.daemon = True
     dash_thread.start()
     webbrowser.open_new('http://127.0.0.1:8050/')
 
-# Functions that is called when pressing the show graph button.
+
 def show_graph():
+    """Functions that is called when pressing the show graph button."""
     if components_checked:
         create_dash_app(components_name, data, components_checked)
     else:
         messagebox.showinfo("No component selected", "Please select atleast one component to plot.")
 
-# Class for the how to use window with the video tutorial.
+
 class ToplevelWindow(customtkinter.CTkToplevel):
+    """Class for the how to use window with the video tutorial."""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.geometry("700x300")
@@ -170,20 +173,25 @@ class ToplevelWindow(customtkinter.CTkToplevel):
         self.label = customtkinter.CTkLabel(self, text=help_text,justify="left")
         self.label.pack(padx=20, pady=20)
 
-        self.button_show_graph = customtkinter.CTkButton(master=self, command=self.play_video, text="See video tutorial")
+        self.button_show_graph = customtkinter.CTkButton(master=self,
+                                                         command=self.play_video,
+                                                         text="See video tutorial")
         self.button_show_graph.pack(pady=10, padx=10)
 
 
     def play_video(self):
+        """Plays the tutorial video"""
         startfile(HELP_VIDEO_FILE)
 
-# Class for the main app and main window
+
 class App(customtkinter.CTk):
+    """Class for the main app and main window"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.checkbox_vars = []
         self.toplevel_window = None
+        self.scrollable_frame = None
 
         self.geometry("400x500")
         self.title("LMT csv visualizer")
@@ -201,46 +209,63 @@ class App(customtkinter.CTk):
         self.frame_1 = customtkinter.CTkFrame(master=self)
         self.frame_1.pack(pady=20, padx=40, fill="both", expand=True)
 
-        main_label = customtkinter.CTkLabel(master=self.frame_1, justify=customtkinter.LEFT, text="Visualize csv data",font=customtkinter.CTkFont(size=20, weight="bold"),bg_color="transparent")
+        main_label = customtkinter.CTkLabel(master=self.frame_1, justify=customtkinter.LEFT,
+                                            text="Visualize csv data",
+                                            font=customtkinter.CTkFont(size=20, weight="bold"),
+                                            bg_color="transparent")
         main_label.pack(pady=10, padx=10)
 
-        button_explore = customtkinter.CTkButton(master=self.frame_1, command=lambda: browse_files(self), text="Browse files")
+        button_explore = customtkinter.CTkButton(master=self.frame_1,
+                                                 command=lambda: browse_files(self),
+                                                 text="Browse files")
         button_explore.pack(pady=10, padx=10)
 
-        button_show_graph = customtkinter.CTkButton(master=self.frame_1, command=show_graph, text="Show graph")
+        button_show_graph = customtkinter.CTkButton(master=self.frame_1,
+                                                    command=show_graph, text="Show graph")
         button_show_graph.pack(pady=10, padx=10)
 
-        button_help = customtkinter.CTkButton(master=self.frame_1, command=self.open_toplevel, text="How to use")
+        button_help = customtkinter.CTkButton(master=self.frame_1,
+                                              command=self.open_toplevel, text="How to use")
         button_help.pack(pady=10, padx=10)
 
-        button_exit = customtkinter.CTkButton(master=self.frame_1, command=quit, text="Exit")
+        button_exit = customtkinter.CTkButton(master=self.frame_1,
+                                              command=quit, text="Exit")
         button_exit.pack(pady=10, padx=10)
 
-    # Makes checkboxes depending of how many componentes are in the csv file. 
+
     def create_dynamic_checkboxes(self, components_name):
+        """Makes checkboxes depending of how many componentes are in the csv file."""
         self.scrollable_frame = customtkinter.CTkScrollableFrame(master=self.frame_1, height=200)
         self.scrollable_frame.pack(fill="both", expand=True)
 
-        scroll_window_label = customtkinter.CTkLabel(master=self.scrollable_frame, justify=customtkinter.LEFT, text="Select what component(s) to visualize",font=customtkinter.CTkFont(size=14, weight="bold"))
+        scroll_window_label = customtkinter.CTkLabel(master=self.scrollable_frame,
+                                                     justify=customtkinter.LEFT,
+                                                     text="Select what component(s) to visualize",
+                                                     font=customtkinter.CTkFont(size=14,
+                                                                                weight="bold"))
         scroll_window_label.pack(pady=10, padx=10)
 
         for idx, name in enumerate(components_name):
             checkbox_var = IntVar()
-            checkbox_1 = customtkinter.CTkCheckBox(master=self.scrollable_frame, text=name, variable=checkbox_var, command=lambda index=idx: self.on_checkbox_change(index))
+            checkbox_1 = customtkinter.CTkCheckBox(master=self.scrollable_frame, text=name,
+                                                   variable=checkbox_var,
+                                                   command=lambda index=idx:
+                                                   self.on_checkbox_change(index))
 
             checkbox_1.pack(pady=10, padx=10)
             self.checkbox_vars.append(checkbox_var)
 
-    # Makes a list for every checkbox that is crossed.
-    def on_checkbox_change(self,index):
 
+    def on_checkbox_change(self,index):
+        """Makes a list for every checkbox that is crossed."""
         if self.checkbox_vars[index].get() == 1:
             components_checked.append(index)
         else:
             components_checked.remove(index)
 
-    # Opens the how to use page
+
     def open_toplevel(self):
+        """Opens the how to use page"""
         if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
             self.toplevel_window = ToplevelWindow(self)
         else:
@@ -248,16 +273,18 @@ class App(customtkinter.CTk):
 
         self.toplevel_window.lift()
 
-    # Stops the app/server
+
     def quit(self):
+        """Stops the app/server"""
         if hasattr(self, "dash_app"):
             self.dash_app.server.stop()  # Gracefully stop the Flask server
             time.sleep(1)
-        
+
         os.kill(os.getpid(), signal.SIGTERM)
-        
-        
+
+
 def main():
+    """Main func to start the UI"""
     app = App()
     app.mainloop()
 
